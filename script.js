ef4d95673951e41a213d6f3a1008ce4bf1bcc513
Imomali <fayzullayevimomali@mail.ru>
2023-02-19 00:49:21 +0500

    Added js scripts

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+//            Bankist App             //
+
+//               Data
+
+const accaunt1 = {
+    owner: 'Imomali Fayzullayev',
+    movements: [200, 450, -400, -650, 130, 70, 1300],
+    interestRate: 1.2,
+    pin: 1111,
+};
+
+
+const accaunt2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, 3210, 1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
+const accaunt3 = {
+    owner: 'Steven Thomas William',
+    movements: [200, -200, 340, -20, 50, 400, -460],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accaunt4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 3333,
+};
+
+const accaunts = [accaunt1, accaunt2, accaunt3, accaunt4];
+
+//                  Elements          //
+
+
+
+
+
