4878326df326b29e5198db0e8d54a4684ed80859
Imomali <fayzullayevimomali@mail.ru>
2023-02-22 05:49:08 +0500

    Added all elements from page

--- script.js
+++ script.js
@@ -36,6 +36,36 @@ const accaunt4 = {
 const accaunts = [accaunt1, accaunt2, accaunt3, accaunt4];
 
 //                  Elements          //
+const labelWelcome = document.querySelector('.welcome');
+const labelData = document.querySelector('.date');
+const labeleBalance = document.querySelector('.balance__value');
+const labalSumIn = document.querySelector('.summary__value--in');
+const labelSumOut  = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--in');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btm--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn-sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+//                         gooo               //
+
+
+
+
 
 
 
