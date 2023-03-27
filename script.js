44f78971ff0a10c62b800ffc209c9074aca0ddb0
Fayzullayev Imomali <fayzullayevimomali@mail.ru>
2023-03-27 18:00:19 +0500

    working with numbers

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const accaunt1 = {
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2,
     pin: 1111,
+    currency: 'UZS'
 };
 
 
@@ -17,6 +18,18 @@ const accaunt2 = {
     movements: [5000, 3400, -150, -790, 3210, 1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
+    movementsDates: [
+        '2019-11-01T13:15:33.035Z',
+        '2019-11-30T09:48:16.867Z',
+        '2019-12-25T06:04:23.907Z',
+        '2020-01-25T14:18:46.235Z',
+        '2020-02-05T16:33:06.386Z',
+        '2020-04-10T14:43:26.374Z',
+        '2020-06-25T18:49:59.371Z',
+        '2020-07-26T12:01:20.894Z',
+      ],
+      currency: 'USD',
+      locale: 'en-US',
 };
 
 const accaunt3 = {
@@ -24,6 +37,7 @@ const accaunt3 = {
     movements: [200, -200, 340, -20, 50, 400, -460],
     interestRate: 1,
     pin: 4444,
+    currency: 'EUR'
 };
 
 const accaunt4 = {
@@ -31,6 +45,7 @@ const accaunt4 = {
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 3333,
+    currency: 'EUR'
 };
 
 const accaunt5 = {
@@ -38,6 +53,18 @@ const accaunt5 = {
     movements: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 5555,
+    movementsDates: [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:42:02.383Z',
+        '2020-01-28T09:15:04.904Z',
+        '2020-04-01T10:17:24.185Z',
+        '2020-05-08T14:11:59.604Z',
+        '2020-05-27T17:01:17.194Z',
+        '2020-07-11T23:36:17.929Z',
+        '2020-07-12T10:51:36.790Z',
+      ],
+      currency: 'EUR',
+      locale: 'pt-PT', // de-DE
 };
 
 const accaunts = [accaunt1, accaunt2, accaunt3, accaunt4, accaunt5];
@@ -83,7 +110,7 @@ const displayMovements  = function (movements, sort = false) {
         const html = `
             <div class="movements__row">
                 <div class="movements__type movements__type--${type}">${type}</div>
-                <div class="movements__value">${mov} UZS</div>
+                <div class="movements__value">${mov} ${currentAccaunt.currency}</div>
             </div>
         `;
         containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -95,7 +122,7 @@ const calcDisplayBalance = function (accaunt) {
     accaunt.balance = accaunt?.movements.reduce((accum, current)=> {
         return accum + current;
     },0);
-    labeleBalance.textContent = `${accaunt.balance} UZS`;
+    labeleBalance.textContent = `${accaunt.balance} ${currentAccaunt.currency}`;
 }
 
 
@@ -152,7 +179,7 @@ btnLogin.addEventListener('click', (e)=> {
     currentAccaunt = accaunts.find((acc)=> acc.username === inputLoginUsername.value);
     
 
-    if(currentAccaunt?.pin === Number(inputLoginPin.value)) {
+    if(currentAccaunt?.pin === +(inputLoginPin.value)) {
     // Display UI and message
         labelWelcome.textContent = `Welcome back, ${currentAccaunt.owner.split(' ')[0]}`;
         containerApp.style.opacity = 100;
@@ -171,7 +198,7 @@ btnLogin.addEventListener('click', (e)=> {
 
 btnTransfer.addEventListener('click', (e)=> {
     e.preventDefault();
-    const amount = Number(inputTransferAmount.value);
+    const amount = +(inputTransferAmount.value);
     const receiverAcc = accaunts.find((acc) => acc.username === inputTransferTo.value);
     
     if( amount > 0 && 
@@ -191,7 +218,7 @@ btnTransfer.addEventListener('click', (e)=> {
 btnClose.addEventListener('click', (e)=> {
     e.preventDefault();
     if(inputCloseUsername?.value === currentAccaunt.username
-        && Number(inputClosePin?.value) === currentAccaunt?.pin){
+        && +(inputClosePin?.value) === currentAccaunt?.pin){
             const index = accaunts.findIndex(acc=> acc.username === currentAccaunt.username)
             accaunts.splice(index, 1);
             //Hide UI 
@@ -225,7 +252,43 @@ btnSort.addEventListener('click', function(e) {
 });
 const movementsArr = [232, -2334, -130, 12000, -6840, -9000, 5600];
 
-alert('Hello new sction');
+console.log(23 === 23.0);
+
+//Base ten => 10 - 0 to 9
+//Binar base 2 - 0 and 1
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+console.log(Number('233'));
+console.log(+'333');
+
+//Parsing
+
+console.log(Number.parseInt('50px', 10));
+
+console.log(Number.parseInt('e444', 10));
+
+console.log(parseFloat('34.5rem', ));
+
+
+// IsNaN checking any value is not a number!
+
+console.log(isNaN(23));
+console.log(isNaN('y2333'));
+console.log(isNaN(23 / 0));
+console.log(isNaN(+'56x'));
+
+
+console.log(Number.isFinite(20));
+console.log(Number.isFinite(+'333'));
+console.log(Number.isFinite(23.5));
+
+//               Math and Round
+
+
+
+
 
 
 
