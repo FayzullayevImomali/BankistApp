b1c712283aacbb2058e5bd411c491751466c7890
Fayzullayev Imomali <fayzullayevimomali@mail.ru>
2023-03-29 17:59:46 +0500

    date

--- script.js
+++ script.js
@@ -9,7 +9,17 @@ const accaunt1 = {
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2,
     pin: 1111,
-    currency: 'UZS'
+    movementsDates: [
+        '2019-11-01T13:15:33.035Z',
+        '2019-11-30T09:48:16.867Z',
+        '2019-12-25T06:04:23.907Z',
+        '2020-01-25T14:18:46.235Z',
+        '2020-02-05T16:33:06.386Z',
+        '2020-04-10T14:43:26.374Z',
+        '2020-06-25T18:49:59.371Z',
+        '2020-07-26T12:01:20.894Z',
+    ],
+    currency: 'UZS'    
 };
 
 
@@ -391,6 +401,38 @@ console.log(23 === 23.0);
 
 
 //                            Creating Date ()                   //
+
+// //Create a date 
+// const now = new Date();
+
+// console.log(now);
+// console.log(new Date('Aug 02 2020 18:05:41'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(accaunt1.movementsDates[3]));
+
+// //
+// console.log(new Date(2037, 11, 19, 15, 23, 5)); // new Date() constructure counts date
+// // not from none its counts date from zero
+
+// //Working with dates
+
+const future = new Date(2037, 11, 19, 15, 23, 5);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+
+console.log(future.toISOString());
+
+console.log(future.getTime());
+console.log(new Date(2144830985000));
+
+console.log(Date.now());
+
 
 
 
